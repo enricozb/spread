555e8f7129ea20323d22610b7d82f3c83bbc2981
Enrico Zandomeni Borba <enricozb@gmail.com>
2026-03-23 16:33:21 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "spread";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    crane.url = "github:ipetkov/crane";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+      crane,
+      treefmt-nix,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay.overlays.default ];
+        };
+
+        rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        craneLib = (crane.mkLib pkgs).overrideToolchain (_: rustToolchain);
+        treefmt = treefmt-nix.lib.evalModule pkgs {
+          projectRootFile = "flake.nix";
+          programs.rustfmt.enable = true;
+          programs.nixfmt.enable = true;
+        };
+      in
+      {
+        devShells.default = craneLib.devShell { };
+
+        formatter = treefmt.config.build.wrapper;
+      }
+    );
+}
